dc1087a0bed7808086c3f56b86a3493d92534c55
htliew0128 <ghtliew@gmail.com>
2016-07-25 14:53:18 +0800

    add exception intent

--- apps/testSkill/index.js
+++ apps/testSkill/index.js
@@ -6,8 +6,15 @@ module.change_code = 1;
 // Define an alexa-app
 var app = new alexa.app('hello-oracle');
 app.launch(function(req,res) {
-	res.say("Hello Oracle!!");
+	res.say("Welcome to Oracle manufacturing!");
 });
+app.intent('GetExceptionIntent', {
+		"slots":{"SHIFT":"LITERAL"}
+		,"utterances":["{Get|Obtain|Find|Retrieve} {today|tomorrow|yesterday|past due|current shift|next shift|SHIFT} {exception|exceptions}"]
+	},function(req,res) {
+		res.say('You have two exceptions '+req.slot('SHIFT'));
+	}
+);
 app.intent('NameIntent', {
 		"slots":{"NAME":"LITERAL","AGE":"NUMBER"}
 		,"utterances":["{My name is|my name's} {matt|hong|bob|bill|jake|nancy|mary|jane|NAME} and I am {1-100|AGE}{ years old|}"]
@@ -19,7 +26,7 @@ app.intent('AgeIntent', {
 		"slots":{"AGE":"NUMBER"}
 		,"utterances":["My age is {1-100|AGE}"]
 	},function(req,res) {
-		res.say('Oracle age is '+req.slot('AGE'));
+		res.say('Your age is '+req.slot('AGE'));
 	}
 );
 module.exports = app;
